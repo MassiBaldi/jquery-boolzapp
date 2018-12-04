c858fec841c51028c564abc2160229aeed63e92d
MassiBaldi <massibaldi85@gmail.com>
2018-12-04 16:40:05 +0100

    Risolto, funziona... manca rispsota e aggiungere orario

--- main.js
+++ main.js
@@ -4,23 +4,13 @@ $(document).ready(function(){
 
     if (event.which == 13){
       var testoValue = $('#messaggio').val();
-      var testoTemplate = $('.template .messaggiV .verde');
+      var testoTemplate = $('.template .messaggiV').clone();
 
-      testoTemplate.text(testoValue);
-
+      testoTemplate.children('.verde').text(testoValue);
+      
       $('.chat').append(testoTemplate)
       $('#messaggio').val('');
     }
   });
-
-  // $('.mic').click(function(){
-  //   // alert('ciao');
-  //   var testo = $('#messaggio').val();
-  //   var smsClonato = $('.template .verde').clone();
-  //   smsClonato.text(testo);
-  //   $('.chat').append(smsClonato);
-  //   $(smsClonato).addClass('active');
-  //   $('#messaggio').val('');
-  // });
 
 });

74f2380f3a41c785635347b0e6abf5b3cf4edfb5
MassiBaldi <massibaldi85@gmail.com>
2018-12-04 16:27:28 +0100

    non funziona

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
 
-  $('.mic').click(function(){
-    // alert('ciao');
-    var testo = $('#messaggio').val();
-    var smsClonato = $('.template .verde').clone();
-    smsClonato.text(testo);
-    $('.chat').append(smsClonato);
-    $(smsClonato).addClass('active');
-    $('#messaggio').val('');
+  $('#messaggio').keypress(function(event){
+
+    if (event.which == 13){
+      var testoValue = $('#messaggio').val();
+      var testoTemplate = $('.template .messaggiV .verde');
+
+      testoTemplate.text(testoValue);
+
+      $('.chat').append(testoTemplate)
+      $('#messaggio').val('');
+    }
   });
+
+  // $('.mic').click(function(){
+  //   // alert('ciao');
+  //   var testo = $('#messaggio').val();
+  //   var smsClonato = $('.template .verde').clone();
+  //   smsClonato.text(testo);
+  //   $('.chat').append(smsClonato);
+  //   $(smsClonato).addClass('active');
+  //   $('#messaggio').val('');
+  // });
 
 });

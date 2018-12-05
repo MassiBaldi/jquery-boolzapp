f069de5f20882c2f830e8898b74e7e4df9ac7c21
MassiBaldi <massibaldi85@gmail.com>
2018-12-05 12:26:56 +0100

    search funziona... impostata ora esatta, avendo aggiunto 2span non riesco ad andare a prendere la classe per "spararla" in chat

--- main.js
+++ main.js
@@ -1,16 +1,51 @@
 $(document).ready(function(){
-
   $('#messaggio').keypress(function(event){
-
     if (event.which == 13){
       var testoValue = $('#messaggio').val();
       var testoTemplate = $('.template .messaggiV').clone();
 
-      testoTemplate.children('.verde').text(testoValue);
-      
-      $('.chat').append(testoTemplate)
+      testoTemplate.children('.verde > .messaggioUtente').text(testoValue);
+      testoTemplate.children('.verde > .oraEsatta').text(ora);
+
+      var ora = (new Date().getHours() + ':' + aggiungiZero(new Date().getMinutes()));
+      console.log(ora);
+
+      function aggiungiZero(zero) {
+        if (zero < 10) {
+            zero = "0" + zero;
+        }
+        return zero;
+      }
+
+      $('.chat').append(testoTemplate);
       $('#messaggio').val('');
+
+      setTimeout(function(){
+        var rispostaTemplate = $('.template .messaggiB').clone();
+        rispostaTemplate.children('.bianco').text('Ok');
+        $('.chat').append(rispostaTemplate);
+      },1000);
     }
   });
+
+  $('#cercaChat').keyup(function() {
+
+    var nomeCercato = $('#cercaChat').val().toLowerCase();
+    console.log(nomeCercato);
+
+    $('.single_chat').each(function(){
+
+      var thisName = $(this).find('#name').text().toLowerCase();
+
+      if (thisName.includes(nomeCercato)) {
+        $(this).show();
+      }
+      else {
+        $(this).hide();
+      }
+    });
+
+  });
+
 
 });
